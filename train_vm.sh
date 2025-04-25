78a44354aac8d6de66cfe2f6af7d5dd18fac4bbc
6000ada <yzhang111@tulane.edu>
2025-04-25 13:18:26 -0500

    update

--- train_vm.sh
+++ train_vm.sh
@@ -8,8 +8,8 @@ do
     CUDA_VISIBLE_DEVICES=$1 python train_vm.py --model $model --seed $seed --num_classes 30\
     --batch_size 256 --epochs 200 --lr 0.01\
     --data_dir /data/eSkin_Data/All_Images\
-    --train_csv_path /media/janet/nips25_benchmark/classification_splits/random/test_ratio=${ratio}/train.csv\
-    --test_csv_path /media/janet/nips25_benchmark/classification_splits/random/test_ratio=${ratio}/test.csv
+    --train_csv_path ./splits/random/test_ratio=${ratio}/train.csv\
+    --test_csv_path ./splits/random/test_ratio=${ratio}/test.csv
 done
 done
 done
@@ -24,8 +24,8 @@ do
     CUDA_VISIBLE_DEVICES=$1 python train_vm.py --model $model --seed $seed --num_classes 30\
     --batch_size 256 --epochs 200 --lr 0.01\
     --data_dir /data/eSkin_Data/All_Images\
-    --train_csv_path /media/janet/nips25_benchmark/classification_splits/uniform/test_number=5/train.csv\
-    --test_csv_path /media/janet/nips25_benchmark/classification_splits/uniform/test_number=5/test.csv\
+    --train_csv_path ./splits/uniform/test_number=5/train.csv\
+    --test_csv_path ./splits/uniform/test_number=5/test.csv\
     # --reweight
 done
 done
